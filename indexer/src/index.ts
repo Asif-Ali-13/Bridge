34932bdd455545594b6510ddfc8e20bcc1fd9813
Asif Ali <aali9548702@gmail.com>
2025-11-06 04:01:05 +0530

    fix: correct variable name from CELLO to CELO and improve event listener formatting

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -6,13 +6,13 @@ import { ABI } from "./contract/ABI";
 import { listenToBridgeEvents } from "./worker/eventListener";
 import { startProcessingQueue } from "./worker/processor";
 
-const providerCELLO = new JsonRpcProvider(process.env.CELLO_RPC!);
+const providerCELO = new JsonRpcProvider(process.env.CELO_RPC!);
 const providerSEPOLIA = new JsonRpcProvider(process.env.SEPOLIA_RPC!);
 
-const contractCELLO = new Contract(
-    process.env.BRIDGE_CONTRACT_ADDRESS_CELLO!,
+const contractCELO = new Contract(
+    process.env.BRIDGE_CONTRACT_ADDRESS_CELO!,
     ABI,
-    providerCELLO
+    providerCELO
 );
 
 const contractSEPOLIA = new Contract(
@@ -22,8 +22,49 @@ const contractSEPOLIA = new Contract(
 );
 
 // Start Event Listeners
-setInterval(() => listenToBridgeEvents(providerCELLO, contractCELLO, "CELLO"), 5000);
-setInterval(() => listenToBridgeEvents(providerSEPOLIA, contractSEPOLIA, "SEPOLIA"), 5000);
+setInterval(async () => 
+    {
+        try {
+            await listenToBridgeEvents(providerCELO, contractCELO, "CELO");
+        } catch (error) {
+            console.error("Error in CELO event listener:", error);
+        }
+    }, 5000
+);
+
+setInterval(async() => 
+    {
+        try {
+            await listenToBridgeEvents(providerSEPOLIA, contractSEPOLIA, "SEPOLIA");
+        } catch (error) {
+            console.error("Error in SEPOLIA event listener:", error);
+        }
+    }, 5000
+);
 
 // Start Queue Processor
 startProcessingQueue();
+
+
+import { prisma } from "./shared/prisma";
+import { bridgeQueue } from "./shared/redis";
+
+process.on("SIGINT", gracefulShutdown);     // Ctrl + C
+process.on("SIGTERM", gracefulShutdown);    // Docker stop
+process.on("beforeExit", gracefulShutdown); // Docker stop
+
+async function gracefulShutdown() {
+    console.log("\nshutting down indexer...");
+
+    try {
+        await prisma.$disconnect();
+        if (bridgeQueue) await bridgeQueue.close();
+        console.log("shutdown complete.");
+    } 
+    catch (err) {
+        console.error("Error during shutdown:", err);
+    } 
+    finally {
+        process.exit(0);
+    }
+}
