57f699944e3244cc822efafdbb2372601e72d01d
Asif Ali <aali9548702@gmail.com>
2025-11-06 03:49:43 +0530

    refactor: standardize property formatting in ABI definition

--- indexer/src/contract/ABI.ts
+++ indexer/src/contract/ABI.ts
@@ -1,56 +1,44 @@
 export const ABI = [
     {
-        anonymous: false,
-        inputs: [
+        "anonymous": false,
+        "inputs": [
             {
-                indexed: false,
-                internalType: "contract IERC20",
-                name: "",
-                type: "address",
+                "indexed": false,
+                "internalType": "contract IERC20",
+                "name": "",
+                "type": "address"
             },
             {
-                indexed: false,
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
             },
             {
-                indexed: false,
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
+                "indexed": false,
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
         ],
-        name: "Bridge",
-        type: "event",
+        "name": "Bridge",
+        "type": "event"
     },
     {
-        inputs: [
+        "inputs": [
             {
-                internalType: "contract IERC20",
-                name: "_tokenAddress",
-                type: "address",
+                "internalType": "contract IERC20",
+                "name": "_tokenAddress",
+                "type": "address"
             },
-            {
-                internalType: "address",
-                name: "_to",
-                type: "address",
-            },
-            {
-                internalType: "uint256",
-                name: "_amount",
-                type: "uint256",
-            },
-            {
-                internalType: "uint256",
-                name: "_nonce",
-                type: "uint256",
-            },
+            { "internalType": "address", "name": "_to", "type": "address" },
+            { "internalType": "uint256", "name": "_amount", "type": "uint256" },
+            { "internalType": "uint256", "name": "_nonce", "type": "uint256" }
         ],
-        name: "redeem",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
+        "name": "redeem",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
 ];
   
