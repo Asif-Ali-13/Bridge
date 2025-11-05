edb7d461150da048471f024ec1a0db3cf2c82072
Asif Ali <aali9548702@gmail.com>
2025-11-06 03:13:42 +0530

    refactor: streamline ABI definitions for TOKEN_ALLOWANCE and BRIDGE

--- frontend/src/utils/account.ts
+++ frontend/src/utils/account.ts
@@ -10,73 +10,41 @@ export const SEPOLIA_TOKEN = `${import.meta.env.VITE_SEPOLIA_TOKEN}` as Address;
 
 export const TOKEN_ALLOWANCE_ABI = [
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
+    "inputs": [
+      { "internalType": "address", "name": "owner", "type": "address" },
+      { "internalType": "address", "name": "spender", "type": "address" }
     ],
-    name: "allowance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
+    "name": "allowance",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
+    "inputs": [
+      { "internalType": "address", "name": "spender", "type": "address" },
+      { "internalType": "uint256", "name": "value", "type": "uint256" }
     ],
-    name: "approve",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "approve",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
 ] as const;
 
 export const BRIDGE_ABI = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "contract IERC20",
-        name: "_tokenAddress",
-        type: "address",
+        "internalType": "contract IERC20",
+        "name": "_tokenAddress",
+        "type": "address"
       },
-      {
-        internalType: "uint256",
-        name: "_amount",
-        type: "uint256",
-      },
+      { "internalType": "uint256", "name": "_amount", "type": "uint256" }
     ],
-    name: "bridge",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "bridge",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
 ] as const;
 
